fa11394697f147ae23b83a481d10d9bc3d205d65
nathanknautz <nate.knautz@gmail.com>
2018-01-16 19:47:32 -0600

    updated things

--- runner.rb
+++ runner.rb
@@ -1,7 +1,19 @@
 require 'unirest'
+require 'tty-table'
 
 response = Unirest.get("http://localhost:3000/all_products_url")
 
 products = response.body
 
 puts JSON.pretty_generate(products)
+
+display_products = []
+products.each do |product|
+  display_products << {name: product["name"],
+                      price: product["price"],
+                    }
+end
+
+table = TTY::Table.new ['name','price'], [display_products[0], display_products[1]]
+
+table.render
